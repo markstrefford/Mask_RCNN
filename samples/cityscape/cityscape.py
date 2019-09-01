0c930e9c1dcbe6bf73a82d8e7f32f4381685d0d1
Mark Strefford <markstrefford@gmail.com>
2019-09-01 19:23:10 +0100

    Debugging

--- samples/cityscape/cityscape.py
+++ samples/cityscape/cityscape.py
@@ -12,19 +12,19 @@ Usage: import the module (see Jupyter notebooks for examples), or run from
        the command line as such:
 
     # Train a new model starting from pre-trained COCO weights
-    python3 cityScape.py train --dataset=/path/to/cityScape/dataset --weights=coco
+    python3 cityscape.py train --dataset=/path/to/cityScape/dataset --weights=coco
 
     # Resume training a model that you had trained earlier
-    python3 cityScape.py train --dataset=/path/to/cityScape/dataset --weights=last
+    python3 cityscape.py train --dataset=/path/to/cityScape/dataset --weights=last
 
     # Train a new model starting from ImageNet weights
-    python3 cityScape.py train --dataset=/path/to/cityScape/dataset --weights=imagenet
+    python3 cityscape.py train --dataset=/path/to/cityScape/dataset --weights=imagenet
 
     # Apply color splash to an image
-    python3 cityScape.py splash --weights=/path/to/weights/file.h5 --image=<URL or path to file>
+    python3 cityscape.py splash --weights=/path/to/weights/file.h5 --image=<URL or path to file>
 
     # Apply color splash to video using the last weights you trained
-    python3 cityScape.py splash --weights=last --video=<URL or path to file>
+    python3 cityscape.py splash --weights=last --video=<URL or path to file>
 """
 
 import os
