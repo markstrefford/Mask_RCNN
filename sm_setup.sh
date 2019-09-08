e116cc03b1f691b1eb9256f5ac6181cb6bb09ee1
Mark Strefford <markstrefford@gmail.com>
2019-09-08 13:17:38 +0100

    Debug

--- sm_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-pip install -r requirements.txt
-cd samples/cityscape
-python cityscape.py $@
---
+++ train.py
@@ -0,0 +1,9 @@
+"""
+Sagemaker compatible train.py
+
+TODO - Make generic across samples directory
+
+"""
+
+from samples.cityscape import cityscape
+cityscape.
